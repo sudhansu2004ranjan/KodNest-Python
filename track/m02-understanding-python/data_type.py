b4e191bf92dbbaf02da96a824a75dca8fc87fc59
Sudhansu Ranjan Swain <swainsudhansu04@gmail.com>
2026-08-03 17:26:39 +0530

    regular commit

---
+++ track/m02-understanding-python/data_type.py
@@ -0,0 +1,10 @@
+#create the required variables
+student_name ="Arave"
+course_duration =30
+course_rating = 4.8
+is_enrolled = True
+
+print(type("student_name"))
+print(type("course_duration"))
+print(type("course_rating"))
+print(type("is_enrolled"))
