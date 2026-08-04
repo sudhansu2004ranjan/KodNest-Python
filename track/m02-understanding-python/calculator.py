41a6fe1f22ceb144986246d9dae5a58aedec36b1
Sudhansu Ranjan Swain <swainsudhansu04@gmail.com>
2026-08-04 18:37:15 +0530

    regular commit

---
+++ track/m02-understanding-python/calculator.py
@@ -0,0 +1,20 @@
+# Accept two numbers
+operation=input("Enter the operation:")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Accept the operator
+operation = input("Enter operation (+, -, *, /): ")
+
+# Perform the operation
+if operation == "+":
+    print( num1 + num2)
+elif operation == "-":
+    print( num1 - num2)
+elif operation == "*":
+    print( num1 * num2)
+elif operation == "/":
+    print(num1/num2)
+       
+    
+
