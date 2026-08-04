ddce3ab9eb0bb8085adb69e2291c527086f6a3e9
Sudhansu Ranjan Swain <swainsudhansu04@gmail.com>
2026-08-04 17:59:58 +0530

    regular commit

--- track/m02-understanding-python/data_type.py
+++ track/m02-understanding-python/data_type.py
@@ -4,7 +4,7 @@ course_duration =30
 course_rating = 4.8
 is_enrolled = True
 
-print(type("student_name"))
-print(type("course_duration"))
-print(type("course_rating"))
-print(type("is_enrolled"))
+print(type(student_name))
+print(type(course_duration))
+print(type(course_rating))
+print(type(is_enrolled))
