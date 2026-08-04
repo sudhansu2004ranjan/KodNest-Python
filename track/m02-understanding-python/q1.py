06a3945bba5bea21dfdd279560ff3265795b2ddd
Sudhansu Ranjan Swain <swainsudhansu04@gmail.com>
2026-08-04 18:01:35 +0530

    regular commit

---
+++ track/m02-understanding-python/q1.py
@@ -0,0 +1,12 @@
+Student_name = input("Enter student name: ")
+Student_age = int(input("Enter age: "))
+Course_rating = float(input("Enter course rating: "))
+
+print(f"Student: {Student_name}")
+print(f"Age: {Student_age}")
+print(f"Rating: {Course_rating}")
+
+print(type(Student_name))
+print(type(Student_age))
+print(type(Course_rating))
+
